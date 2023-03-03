4750b009b52b1080d8c3b6722e113e5464173981
Toms Teteris <tt00792@gmail.com>
2023-03-03 09:55:40 +0000

    Add PS Profile file sample

---
+++ Profile.ps1
@@ -0,0 +1,19 @@
+$ESC = [char]27
+
+function Prompt
+{
+    $LocationName = (Get-Item .).Name
+
+    if ((Get-Location).Path -eq "/home/$($env:UserName)")
+    {
+        $LocationName = "~"
+    }
+
+    $UserAtDomain = ""
+    if ($env:COMPUTERNAME -ne $env:USERDOMAIN)
+    {
+        $UserAtDomain = $env:USERDOMAIN + "\"
+    }
+
+    "$ESC[38;2;255;0;200m[" + $env:COMPUTERNAME + "]38;2;255;200;0m[" + $UserAtDomain + $env:UserName + "]$ESC[0m " + $LocationName + " > "
+}
