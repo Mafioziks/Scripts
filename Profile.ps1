e3e8e18c8ae88a4ddd878309fa736b6b2a9977d0
Toms Teteris <tt00792@gmail.com>
2023-03-16 15:34:17 +0000

    Added detailed location description and version information in beginning of PS prompt

--- Profile.ps1
+++ Profile.ps1
@@ -1,4 +1,34 @@
-# Location to place in: ~\Documents\PowerShell
+# FOR PS 7.3 (a.k.a. - Powershell)
+# For example, the PowerShell console supports the following basic profile files. The profiles are listed in precedence order. The first profile has the highest precedence.
+#
+#     All Users, All Hosts
+#         Windows - $PSHOME\Profile.ps1
+#         Linux - /usr/local/microsoft/powershell/7/profile.ps1
+#         macOS - /usr/local/microsoft/powershell/7/profile.ps1
+#     All Users, Current Host
+#         Windows - $PSHOME\Microsoft.PowerShell_profile.ps1
+#         Linux - /usr/local/microsoft/powershell/7/Microsoft.Powershell_profile.ps1
+#         macOS - /usr/local/microsoft/powershell/7/Microsoft.Powershell_profile.ps1
+#     Current User, All Hosts
+#         Windows - $HOME\Documents\PowerShell\Profile.ps1
+#         Linux - ~/.config/powershell/profile.ps1
+#         macOS - ~/.config/powershell/profile.ps1
+#     Current user, Current Host
+#         Windows - $HOME\Documents\PowerShell\Microsoft.PowerShell_profile.ps1
+#         Linux - ~/.config/powershell/Microsoft.Powershell_profile.ps1
+#         macOS - ~/.config/powershell/Microsoft.Powershell_profile.ps1
+
+
+# FOR PS 5.1 (a.k.a. - Windows Powershell)
+# For example, the $PROFILE variable has the following values in the Windows PowerShell console.
+#
+#     Current User, Current Host - $PROFILE
+#     Current User, Current Host - $PROFILE.CurrentUserCurrentHost
+#     Current User, All Hosts - $PROFILE.CurrentUserAllHosts
+#     All Users, Current Host - $PROFILE.AllUsersCurrentHost
+#     All Users, All Hosts - $PROFILE.AllUsersAllHosts
+
+
 
 $ESC = [char]27
 
@@ -14,5 +44,6 @@ function Prompt {
         $UserAtDomain = $env:USERDOMAIN + "\"
     }
 
-    "$ESC[38;2;255;0;200m[" + $env:COMPUTERNAME + "]$ESC[38;2;255;200;0m[" + $UserAtDomain + $env:UserName + "]$ESC[0m " + $LocationName + " > "
+    # $PSVersionTable.PSVersion.Patch - might add for PS 7.3 as for PS 5.1 it does not exist
+    "$ESC[38;2;0;204;34m[$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor)]$ESC[38;2;255;0;200m[" + $UserAtDomain + $env:UserName + "]$ESC[0m " + $LocationName + " > "
 }
